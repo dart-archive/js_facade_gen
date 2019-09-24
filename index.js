6a044c9913b98711b14b0cc758fb0f99adf773de
Derek Xu <derekx@google.com>
2019-09-24 11:20:06 -0700

    Use minimist to rename flag to TranspilerOption property instead of doing it manually

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 const main = require('./build/lib/main.js');
 
-var args =
-    require('minimist')(process.argv.slice(2), {base: 'string', boolean: ['semantic-diagnostics']});
+var args = require('minimist')(process.argv.slice(2), {
+  base: 'string',
+  boolean: ['semantic-diagnostics'],
+  alias: {'semantic-diagnostics': 'semanticDiagnostics'}
+});
 try {
-  args.semanticDiagnositcs = args['semantic-diagnostics'] || false;
-  delete args['semantic-diagnostics'];
   var transpiler = new main.Transpiler(args);
   if (args.destination) console.error('Transpiling', args._, 'to', args.destination);
   transpiler.transpile(args._, args.destination);
